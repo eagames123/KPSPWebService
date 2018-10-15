3324aa1ebe7b8c23ffa4aac81d4564b8e22c395d
Erkan <erkan.salihoglu@hotmail.com>
2018-10-16 00:21:21 +0300

    init

---
+++ KPSPWebService/KPSPWebService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KPSPWebService.KpspService;
+
+namespace KPSPWebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KPSPublicSoapClient kpsPublicSoapClient = new KPSPublicSoapClient();
+
+            //Console.WriteLine(kpsPublicSoapClient.TCKimlikNoDogrula(Long TC NO, "String Büyük Harf Ad".ToUpper(), "String Büyük Harf Soyad".ToUpper(), int Doğum Yılı));
+
+            Console.ReadLine();
+        }
+    }
+}
